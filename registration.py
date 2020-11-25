305c1e74efba25c1f52e45a1efe76df621df38e6
sp172900 <65318960+sp172900@users.noreply.github.com>
2020-11-25 17:35:00 -0500

    Update registration.py

--- registration.py
+++ registration.py
@@ -1,4 +1,5 @@
 from getpass import getpass
+import hashlib
 
 def registration():
 	f_name = input("Enter First Name: ")
@@ -6,24 +7,25 @@ def registration():
 	employee_id = input("Enter Employee ID: ")
 
 
+	# check database for user
 	sql_select_Query = "select * from Employees"
-    cursor = connection.cursor()
-    cursor.execute(sql_select_Query)
-    records = cursor.fetchall()
-    found = false
+	cursor = connection.cursor()
+	cursor.execute(sql_select_Query)
+	records = cursor.fetchall()
+	found = false
 
-    for row in records:
-    	if row[0] == employee_id and row[1] == f_name and row[2] == l_name:
-    		found = true
-    		break
+	for row in records:
+		if row[0] == employee_id and row[1] == f_name and row[2] == l_name:
+			found = true
+			break
 
-    # check if user is an employee
-    if found == false:
-    	print("You are not an employee. You cannot register.")
-    	return
+	# check if user is an employee
+	if found == false:
+		print("You are not an employee. You cannot register.")
+		return
 
-    # check if user is registered
-	if row[4] == false:
+	# check if user is registered
+	if row[4] == 1:
 		print("You are already registered. Please proceed to the login screen.")
 		return
 
@@ -36,17 +38,17 @@ def registration():
 		password = getpass("Enter Password: ")
 		confirm_pass = getpass("Confirm Password: ")
 
+	hashed_pass = hashlib.sha256()
+	hashed_pass.update(password)
+	hashed_pass.digest()
 
 	# save password in database
 	cursor.execute('''
-            INSERT INTO Employees (password)
-            VALUES
-            (password),
-            ''')
+		INSERT INTO Employees (password)
+		VALUES
+		(hashed_pass.hexdigest()),
+		''')
 
 
 	print("Registration Successful!")
 	return
-
-
-registration()
