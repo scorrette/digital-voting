ce938654cd4466dfc0ddd2ce777895c09881034b
eolanday <eolanday@gmail.com>
2020-12-06 15:54:12 -0500

    multiple ballots per message and doing signature and encryption using local pem files

--- authenticatorFile.py
+++ authenticatorFile.py
@@ -1,5 +1,7 @@
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
+from Crypto.Hash import SHA256
+from Crypto.Signature.pkcs1_15 import PKCS115_SigScheme
 from base64 import b64encode,b64decode
 import mysql.connector
 
@@ -10,44 +12,69 @@ class Authenticator:
         self.auth_private_key = self.auth_key.exportKey("PEM")
         self.auth_public_key = self.auth_key.publickey().exportKey("PEM")
         self.auth_cipher = PKCS1_OAEP.new(self.auth_key)
+
     def getKey(self):
         print("")
     def sendKey(self):
         print()
-    def authenticate(self,message):
-        plainMessage = self.auth_cipher.decrypt(message)
-        [id_byte,cipherVote_2] = message.split(b',,,,,')
-        userID = id_byte.decode("utf-8")
-        mydb = mysql.connector.connect(
-            host = 'ss3010.rutgers-sci.domains',
-            user = 'ssrutge4_user1',
-            password = "yourpassword",
-            database = 'ssrutge4_ECE424'
-        )
-        dbCursor = mydb.cursor()
-        queryString = "select has_registered from Employees where id = " + userID
-        dbCursor.execute(queryString)
-        result = dbCursor.fetchone()
-        if result[0] == "True":
-            dbCursor.close()
-            mydb.close()
-            raise Exception("User Already Voted")
-        else:
-            #pass cipherVote_2 to Counter
-            queryString = "update Employees set has_registered = True where id = " + userID
+    def authenticate(self,messages):
+        encryptedVoteList = ""
+        deliminator = ",,,,,".encode("utf-8")
+        for message in messages:
+            plainMessage = self.auth_cipher.decrypt(message)
+            [id_byte,cipherVote_2] = plainMessage.split(b',,,,,')
+            userID = id_byte.decode("utf-8")
+            mydb = mysql.connector.connect(
+                host = 'ss3010.rutgers-sci.domains',
+                user = 'ssrutge4_user1',
+                password = "yourpassword",
+                database = 'ssrutge4_ECE424'
+            )
+            dbCursor = mydb.cursor()
+            queryString = "select has_registered from Employees where id = " + userID
             dbCursor.execute(queryString)
-            dbCursor.close()
-            mydb.close()
-            signature = b64encode(RSA.sign(cipherVote_2,self.auth_private_key),"SHA-512")
-            auth_cipher = PKCS1_OAEP.new(getKey())
-            cipherVote_3 = auth_cipher.encrypt(cipherVote_2)
-            return [cipherVote_3,signature]
+            result = dbCursor.fetchone()
+            if result[0] == "True":
+                dbCursor.close()
+                mydb.close()
+                raise Exception("User Already Voted")
+            else:
+                #pass cipherVote_2 to Counter
+                queryString = "update Employees set has_registered = True where id = " + userID
+                dbCursor.execute(queryString)
+                dbCursor.close()
+                mydb.close()
+                #signature = b64encode(RSA.sign(cipherVote_2,self.auth_private_key),"SHA-512")
+                #auth_cipher = PKCS1_OAEP.new(getKey())
+                #cipherVote_3 = auth_cipher.encrypt(cipherVote_2)
+                #return [cipherVote_3,signature]
+                if(encryptedVoteList ==""):
+                    encryptedVoteList = cipherVote_2
+                else:
+                    encryptedVoteList = encryptedVoteList + deliminator + cipherVote_2
+                continue
+        create_Signature(encryptedVoteList)
+        create_Encrypted_Ballot(encryptedVoteList)
 
-def getKey():
-    new_key = RSA.generate(4096,e=65537)
-    private_key = new_key.exportKey("PEM")
-    public_key = new_key.publickey().exportKey("PEM")
-    return public_key
+def create_Signature(vote):
+    # Sign the ballot using authenticator's private key
+    auth_private_key = RSA.importKey(open('auth_private_key.pem').read())
+    ballot = vote.encode("utf-8")
+    hash1 = SHA256.new(ballot)
+    signer = PKCS115_SigScheme(auth_private_key)
+    signature = signer.sign(hash1)
+    with open("signature.txt", "wb") as f:
+        f.write(signature)
+
+
+def create_Encrypted_Ballot(vote):
+    # Encrypt ballot using COUNTER's PUBLIC KEY
+    ballot = vote.encode("utf-8")
+    counter_public_key = RSA.importKey(open('counter_public_key.pem').read())
+    counter_cipher = PKCS1_OAEP.new(counter_public_key)
+    encrypted_ballot = counter_cipher.encrypt(ballot)
+    with open("enc_ballot.ballot", "wb") as f:
+        f.write(encrypted_ballot)
 
 def mainTest():
     new_key = RSA.generate(1024,e=65537)
