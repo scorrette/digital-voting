062db2ed1a47164e7d24e62b4220189e12b784df
eolanday <eolanday@gmail.com>
2020-12-06 14:17:06 -0500

    made authenticator into class

--- authenticatorFile.py
+++ authenticatorFile.py
@@ -3,38 +3,45 @@ from Crypto.Cipher import PKCS1_OAEP
 from base64 import b64encode,b64decode
 import mysql.connector
 
-def authenticator(message):
-    auth_key = RSA.generate(4096,e=65537)
-    auth_private_key = auth_key.exportKey("PEM")
-    auth_public_key = auth_key.publickey().exportKey("PEM")
-    auth_cipher = PKCS1_OAEP.new(auth_key)
-    plainMessage = auth_cipher.decrypt(message)
-    [id_byte,cipherVote_2] = message.split(b',,,,,')
-    userID = id_byte.decode("utf-8")
-    mydb = mysql.connector.connect(
-        host = 'ss3010.rutgers-sci.domains',
-        user = 'ssrutge4_user1',
-        password = "yourpassword",
-        database = 'ssrutge4_ECE424'
-    )
-    dbCursor = mydb.cursor()
-    queryString = "select has_registered from Employees where id = " + userID
-    dbCursor.execute(queryString)
-    result = dbCursor.fetchone()
-    if result[0] == "True":
-        dbCursor.close()
-        mydb.close()
-        raise Exception("User Already Voted")
-    else:
-        #pass cipherVote_2 to Counter
-        queryString = "update Employees set has_registered = True where id = " + userID
+class Authenticator:
+
+    def __init__(self):
+        self.auth_key = RSA.generate(4096,e=65537)
+        self.auth_private_key = self.auth_key.exportKey("PEM")
+        self.auth_public_key = self.auth_key.publickey().exportKey("PEM")
+        self.auth_cipher = PKCS1_OAEP.new(self.auth_key)
+    def getKey(self):
+        print("")
+    def sendKey(self):
+        print()
+    def authenticate(self,message):
+        plainMessage = self.auth_cipher.decrypt(message)
+        [id_byte,cipherVote_2] = message.split(b',,,,,')
+        userID = id_byte.decode("utf-8")
+        mydb = mysql.connector.connect(
+            host = 'ss3010.rutgers-sci.domains',
+            user = 'ssrutge4_user1',
+            password = "yourpassword",
+            database = 'ssrutge4_ECE424'
+        )
+        dbCursor = mydb.cursor()
+        queryString = "select has_registered from Employees where id = " + userID
         dbCursor.execute(queryString)
-        dbCursor.close()
-        mydb.close()
-        signature = b64encode(RSA.sign(cipherVote_2,auth_private_key),"SHA-512")
-        auth_cipher = PKCS1_OAEP.new(getKey())
-        cipherVote_3 = auth_cipher.encrypt(cipherVote_2)
-        return [cipherVote_3,signature]
+        result = dbCursor.fetchone()
+        if result[0] == "True":
+            dbCursor.close()
+            mydb.close()
+            raise Exception("User Already Voted")
+        else:
+            #pass cipherVote_2 to Counter
+            queryString = "update Employees set has_registered = True where id = " + userID
+            dbCursor.execute(queryString)
+            dbCursor.close()
+            mydb.close()
+            signature = b64encode(RSA.sign(cipherVote_2,self.auth_private_key),"SHA-512")
+            auth_cipher = PKCS1_OAEP.new(getKey())
+            cipherVote_3 = auth_cipher.encrypt(cipherVote_2)
+            return [cipherVote_3,signature]
 
 def getKey():
     new_key = RSA.generate(4096,e=65537)
